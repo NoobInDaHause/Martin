7eae0bd24377ce256eede5b121e62ceb95930c68
NoobInDaHause <johnjohncedric214@gmail.com>
2026-09-06 16:07:16 +0800

    advanced startup test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 from pathlib import Path
@@ -9,26 +10,32 @@ from Martin import Martin, Settings
 from Martin.interaction import response_or_followup
 
 
-def run_bot() -> None:
+def setup_lopgging():
     logging.basicConfig(
         level=logging.INFO,
         handlers=[
             logging.StreamHandler(),
             logging.FileHandler(Path(__file__).parent / "logs.log", "w"),
         ],
-        format="[{asctime}] [{levelname}] {name:<15}: {message}",
+        format="[{asctime}] [{levelname}] {name:<30}: {message}",
         datefmt="%Y-%m-%d %H:%M:%S",
         style="{",
     )
 
+
+async def run_bot() -> None:
+    setup_lopgging()
+
     load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
     discord.Interaction.response_or_followup = response_or_followup  # Dirty way to add custom functions but it works and does its job
+
     settings = Settings.initialize()
-    bot = Martin(settings)
-    bot.run(token=os.getenv("TOKEN"), log_handler=None)
-    raise SystemExit(bot.exit_code)
+    async with Martin(settings) as bot:
+        await bot.start(token=os.getenv("TOKEN"))
+        return bot.exit_code
 
 
 if __name__ == "__main__":
-    run_bot()
+    exit_code = asyncio.run(run_bot())
+    raise SystemExit(exit_code)
